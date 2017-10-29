415286167b5beeb556e75bf20b325b308a5f2abb
Boanerges <boanerges.martinez@ucr.ac.cr>
2017-10-29 03:25:04 -0600

    Trabajo en modulo fifo, adaptacion a requerimientos
    se cambiaron los define por parametros, se documento el codigo,  se actualizo el test_fifo.v, se copio las versiones del proyecto1 del renameSynths.sh y yosys.tcl para corregir error en ejecucion del make

--- bloques/FIFO/fifo.v
+++ bloques/FIFO/fifo.v
@@ -1,33 +1,29 @@
 `timescale 1ns/1ps
-`define BUF_WIDTH 3    // BUF_SIZE = 16 -> BUF_WIDTH = 4, no. of bits to be used in pointer
-`define BUF_SIZE ( 1<<`BUF_WIDTH )
+//para estaciar se usar el siguiente orden
+//fifo #(.BUF_WIDTH(4)) instancia_fifo1 (asignacion de puertos .clk(clk1),... ) ;
+module fifo #(parameter BUF_WIDTH = 3)//cantidad de bits de direccionamiento a/y posiciones de memoria del fifo
+ (
+  output reg buf_empty, buf_full, almost_full, almost_empty, //banderas de estatus
+  output reg [3:0] buf_out,//port to output the data using pop.
+  output reg [BUF_WIDTH :0] fifo_counter, // number of data pushed in to buffer
+  input clk, rst, wr_en, rd_en, // reset, system clock, write enable and read enable.
+  input [3:0] buf_in//data input to be pushed to buffer
+);
+//Parametros y varibles internas
+// BUF_SIZE = 16 -> BUF_WIDTH = 4, no. of bits to be used in pointer
+parameter BUF_SIZE = ( 1<<BUF_WIDTH ); // 1 << 3 = 100 binario = 8 decimal, 8 posiciones de memoria
+reg [(BUF_WIDTH-1):0]  rd_ptr, wr_ptr; // pointer to read and write addresses
+reg [3:0] buf_mem [(BUF_SIZE-1) : 0]; // Memoria: 4bits * (BUF_SIZE-1)posiciones
 
-module fifo( clk, rst, buf_in, buf_out, wr_en, rd_en, buf_empty, buf_full, fifo_counter );
-
-input                 rst, clk, wr_en, rd_en;
-// reset, system clock, write enable and read enable.
-input [7:0]           buf_in;
-// data input to be pushed to buffer
-output[7:0]           buf_out;
-// port to output the data using pop.
-output                buf_empty, buf_full;
-// buffer empty and full indication
-output[`BUF_WIDTH :0] fifo_counter;
-// number of data pushed in to buffer
-
-reg[7:0]              buf_out;
-reg                   buf_empty, buf_full;
-reg[`BUF_WIDTH :0]    fifo_counter;
-reg[`BUF_WIDTH -1:0]  rd_ptr, wr_ptr;           // pointer to read and write addresses
-reg[7:0]              buf_mem[`BUF_SIZE -1 : 0]; //
-
+//banderas segun conteo de datos en el fifo
 always @(fifo_counter)
 begin
    buf_empty = (fifo_counter==0);
-   buf_full = (fifo_counter== `BUF_SIZE);
+   buf_full = (fifo_counter== BUF_SIZE);
 
 end
 
+//conteo de datos en el fifo ingresados o sacados
 always @(posedge clk or posedge rst)
 begin
    if( rst )
@@ -46,6 +42,7 @@ begin
       fifo_counter <= fifo_counter;
 end
 
+//pop, leer datos desde el fifo
 always @( posedge clk or posedge rst)
 begin
    if( rst )
@@ -61,6 +58,7 @@ begin
    end
 end
 
+//push, escribir datos en el fifo
 always @(posedge clk)
 begin
 
@@ -71,6 +69,7 @@ begin
       buf_mem[ wr_ptr ] <= buf_mem[ wr_ptr ];
 end
 
+//control de punteros de lectura y escritura
 always@(posedge clk or posedge rst)
 begin
    if( rst )
