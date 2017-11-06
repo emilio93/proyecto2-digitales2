ead9803d3bd3950daabddc0e70d66cfb9ba115f9
Robin <weiserfi@outlook.com>
2017-11-06 00:51:06 -0600

    fsm

---
+++ bloques/flowControl/fsm.v
@@ -0,0 +1,142 @@
+`timescale 1ns/1ps
+
+`ifndef fsm
+`define fsm
+
+//Usar mas de un always, usar one Hot o parecido, especificar.
+
+module fsm(
+  input iniciar,
+  input reset,
+  input almost_full,
+  input full,
+  input almost_empty,
+  input empty,
+  input clk,
+  output continuar,
+  output error_full,
+  output pausa,
+  output idle
+);
+
+  // Entradas
+  wire iniciar, reset;
+  wire almost_full;
+  wire full;
+  wire empty;
+  wire clk;
+
+  //Salidas
+  reg error_full;
+  reg pausa;
+  reg continuar;
+  reg idle;
+
+  //Se tienen 7 estados por lo tanto basta representarlos con 3 bits en
+  //Codificacion binario ordinario
+
+  //Variables internas
+  reg [1:0] estados;
+  /*
+  * active7
+  * pause5
+  * error2
+  * rst1
+  * continue6
+  * idle4
+  * init3
+  * */
+  parameter rst = 3'b001,
+	  error = 3'b010,
+	  init = 3'b100,
+	  idleEmpty = 3'b011,
+	  pause = 3'b101,
+	  continue = 3'b110,
+	  active = 3'b111;
+
+  reg [2:0] proximo, actual;
+
+//  always @(posedge clk or negedge reset)begin
+//	  if (!rst) atual <= active;
+//	  else actual <= proximo;
+//end
+
+  always @(posedge clk)begin
+	actual <= proximo;
+	if (rst) actual <= init;
+
+//v	  else actual <= proximo;
+
+//	  always @(actual or full or almost_full or empty or almost_empty or reset or init) begin
+	case(actual)
+
+		active : begin
+			continuar = 0;
+			pausa = 0;
+			error_full = 0;
+			idle = 0;
+			if(full > 0) proximo = error;
+				else if(almost_full > 0) proximo = pausa;
+				else if(empty > 0) proximo = idleEmpty;
+				else if(almost_empty > 0) proximo = continue;
+			else proximo = actual;
+		end
+
+		continue : begin 
+			continuar = 1;
+			pausa = 0;
+			error_full = 0;
+			idle = 0;
+			if(full >0) proximo = error;
+				else proximo =  active;
+		end
+
+		pause : begin 
+			continuar = 0;
+			pausa = 1;
+			error_full = 0;
+			idle = 0;
+			proximo = active;
+		end  
+
+		idleEmpty : begin 
+			continuar = 0;
+			pausa = 0;
+			error_full = 0;
+			idle = 1;
+			if(empty < 0) proximo = active;
+				else proximo = actual;
+		end
+
+		init : begin 
+			continuar = 0;
+			pausa = 0;
+			error_full = 0;
+			idle = 0;
+			if(iniciar) proximo = idleEmpty;
+				else proximo = actual;
+		end
+
+		error : begin  
+			continuar = 0;
+			pausa = 0;
+			error_full = 1;
+			idle = 0;
+			proximo = rst;
+		end
+
+		rst :  begin 
+			continuar = 0;
+			pausa = 0;
+			error_full = 1;
+			idle = 0;
+			proximo = idleEmpty;
+		end
+
+	endcase
+
+end
+
+endmodule // fsm
+`endif
+
---
+++ pruebas/fsm_test.v
@@ -0,0 +1,155 @@
+`timescale 1ns/1ps
+
+`define isTest 1
+
+`include "../lib/cmos_cells.v"
+`include "../bloques/flowControl/fsm.v"
+`include "../build/fsm-sintetizado.v"
+
+module fsm_test();
+
+  reg clk, reset, aFull, full , aEmpty, empty, iniciar;
+  wire continuar, error, pausa, idle;
+  wire continuarSynth, errorSynth, pausaSynth, idleSynth;
+
+  parameter delay = 10;
+
+  fsm fsm(
+	.iniciar(clk),
+	.reset(reset),
+	.almost_full(aFull),
+	.full(full),
+	.almost_empty(aEmpty),
+	.empty(empty),
+	.clk(clk),
+	.continuar(continuar),
+	.error_full(error),
+	.pausa(pausa),
+	.idle(idle)
+  );
+
+  fsm fsmSynth(
+	.iniciar(clk),
+	.reset(reset),
+	.almost_full(aFull),
+	.full(full),
+	.almost_empty(aEmpty),
+	.empty(empty),
+	.clk(clk),
+	.continuar(continuarSynth),
+	.error_full(errorSynth),
+	.pausa(pausaSynth),
+	.idle(idleSynth)
+  );
+
+
+  always # 5 clk = ~clk;
+
+  initial
+  begin
+    $dumpfile("gtkws/fsm_test.vcd");
+    $dumpvars();
+    $display("fsm_test");
+
+    iniciar <= 0;
+    reset <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 1;
+    empty <= 0;
+    full <= 0;
+
+    #delay;
+	
+    clk <= 0;
+    reset <= 1;
+
+    #delay;
+
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 1;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 1;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 1;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 1;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 1;
+    full <= 0;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+    #delay;
+    iniciar <= 0;
+    aFull <= 0;
+    full <= 1;
+    aEmpty <= 0;
+    empty <= 0;
+    reset <= 0;
+
+
+    # 15 $finish;
+  end
+endmodule
