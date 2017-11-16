281045632db1035f3ef0fee8e80cd9a390bf8a43
Emilio Rojas <emilio93@gmail.com>
2017-11-16 17:13:27 -0600

    actualizacion 1 del makefile

--- renameSynths.sh
+++ renameSynths.sh
@@ -6,8 +6,8 @@ for file in ./*-sintetizado.v; do
   moduleName=${file/.\//}
   moduleName=${moduleName/-sintetizado.v/}
   newModuleName=${moduleName}Synth
-  echo "${file}:"
-  echo "    ${moduleName} -> ${newModuleName}"
+  echo "build/${file/.\//}: ${moduleName} -> ${newModuleName}"
   echo "sed -i \"s/module ${moduleName}/module  ${newModuleName}/g\" \"${file}\""
   sed -i "s/module ${moduleName}/module  ${newModuleName}/" "${file}"
+  echo ""
 done
